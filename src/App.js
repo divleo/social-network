c8f0677570ed75a457eea2f01588080d94e99af1
divleo <11596691+divleo@users.noreply.github.com>
2020-04-21 03:13:50 +0300

    Add raw layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <header>
+      <div className="container">Header</div>
+    </header>
+    <main>
+      <div className="container">
+        <div className="row">
+          <aside className="main-sidebar">
+            <ul className="sidebar-navigation">
+              <li>
+                <a href="/">Profile</a>
+              </li>
+              <li>
+                <a href="/">Messages</a>
+              </li>
+              <li>
+                <a href="/">News</a>
+              </li>
+              <li>
+                <a href="/">Music</a>
+              </li>
+              <li>
+                <a href="/">Settings</a>
+              </li>
+            </ul>
+          </aside>
+          <section className="main-content">
+            <p className="profile-background-image">
+              <img
+                src="https://w.wallhaven.cc/full/96/wallhaven-96w8e8.png"
+                alt="Profile background image"
+              />
+            </p>
+            <div>ava + description</div>
+            <div>
+              posts
+              <div>post 1</div>
+              <div>post 2</div>
+            </div>
+          </section>
+        </div>
+      </div>
+    </main>
+    <footer>
+      <div className="container">Footer</div>
+    </footer>
+  </>
+);
 
 export default App;
